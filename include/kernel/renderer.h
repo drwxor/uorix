fcbf412492a09360d856b08fbf28da2e9a0c5cdc
drvxor <drvxor@duck.com>
2026-09-17 22:27:59 +0300

    feat(kernel): make renderer use limine's flanterm

--- include/kernel/renderer.h
+++ include/kernel/renderer.h
@@ -4,14 +4,14 @@
 #define UORIX_RENDERER_H
 
 #include <stdint.h>
+
 #include "kernel/limine.h"
 
-void renderer_init(struct limine_framebuffer *fb);
+void renderer_init(struct limine_framebuffer *fb,struct limine_flanterm_fb_init_params *font_params);
 
 void render_clear(uint32_t color);
 void render_putc(char c);
 void render_puts(const char *s);
-
 void render_printf(const char *fmt, ...);
 
 #endif
